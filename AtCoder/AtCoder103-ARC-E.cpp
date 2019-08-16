9554260033f89b17f3e9c68d497181bfc1524ac2
racsosabe <racso_galvan@hotmail.com>
2019-08-16 17:16:58 -0500

    Tr/ee with comments

---
+++ AtCoder/AtCoder103-ARC-E.cpp
@@ -0,0 +1,159 @@
+#include<bits/stdc++.h>
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ri(x) scanf("%d",&(x))
+#define ri2(x,y) scanf("%d %d",&(x),&(y))
+#define ri3(x,y,z) scanf("%d %d %d",&(x),&(y),&(z))
+#define rll(x) scanf("%lld",&(x))
+#define rll2(x,y) scanf("%lld %lld",&(x),&(y))
+#define rll3(x,y,z) scanf("%lld %lld %lld",&(x),&(y),&(z))
+#define gc(x) ((x) = getchar())
+using namespace::std;
+
+const long double PI = acos(-1);
+const long long MOD = 1000000000 +7;
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<ll,pll> tll;
+typedef pair<int,int> ii;
+typedef pair<int,ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<tll> vtll;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ii> sii;
+typedef set<iii> siii;
+
+ll gcd(ll a, ll b){ return b==0?a:gcd(b,a%b);}
+
+ll add(ll a, ll b, ll m = MOD){
+	if(a >= m) a %= m;
+	if(b >= m) b %= m;
+	if(a < 0) a += m;
+	if(b < 0) b += m;
+	ll res = a+b;
+	if(res >= m or res <= -m) res %= m;
+	if(res < 0) res += m;
+	return res;
+}
+
+ll mul(ll a, ll b, ll m = MOD){
+	if(a >= m) a %= m;
+	if(b >= m) b %= m;
+	if(a < 0) a += m;
+	if(b < 0) b += m;
+	ll res = a*b;
+	if(res >= m or res <= -m) res %= m;
+	if(res < 0) res += m;
+	return res;
+}
+
+ll pow_mod(ll a, ll b, ll m = MOD){
+	ll res = 1LL;
+	a = a%m;
+	while(b){
+		if(b&1) res = mul(res,a,m);
+		b >>= 1;
+		a = mul(a,a,m);
+	}
+	return res;
+}
+
+ll fastexp(ll a, ll b){
+	ll res = 1LL;
+	while(b){
+		if(b&1) res = res*a;
+		b >>= 1;
+		a *= a;
+	}
+	return res;
+}
+
+int gcdExtendido(int a, int b, int *x, int *y){
+	if(a == 0){
+		*x = 0;
+		*y = 1;
+		return b;
+	}
+	int x1, y1;
+	int gcd = gcdExtendido(b%a,a,&x1,&y1);
+	
+	*x = y1-(b/a)*x1;
+	*y = x1;
+	return gcd;
+}
+
+int modInverso(int a, int m){
+	int x, y;
+	int g = gcdExtendido(a,m,&x,&y);
+	if(g!=1) return -1;
+	else return (x%m + m)%m;
+}
+
+/****************************************
+*************P*L*A*N*T*I*L*L*A************
+*****************************************/
+
+/*
+	Author: Racso Galvan
+
+	Idea: 
+		- Interesting graph problem.
+
+		- First, notice that s_n must be 0, since the graph must be a tree.
+
+		  Then, notice that s_1 must be 1, since a tree always has at least one leaf.
+
+		  Finally, notice that if s_i is 1 then s_(n-i) must be 1, too.
+
+		- After checking the former conditions, if the string holds everyone of them
+
+		  then there is an answer.
+
+		  Now, we must consider the sizes of the components to analyze.
+
+		  However, it's not necessary that they are disjoint. Thus, we can
+
+		  just nest all the components in descendant order of sizes.
+
+		  Observation: The extra nodes of the difference of sizes can be
+
+		  joined with the current root and they will continue holding the
+
+		  property since their split will create components of sizes 1 and (n-1)
+
+		  which must hold trivially.
+
+		- Complexity: O(n)
+*/
+
+const int N = 100000+5;
+
+int n;
+string s;
+
+int main(){
+	cin >> s;
+	n = s.size();
+	bool can = (s[0] == '1') & (s[n-1] == '0');
+	for(int i=2; i<n-i; i++){
+		if(s[i-1] != s[n-1-i]) can = false;
+	}
+	if(can){
+		reverse(all(s));
+		int root = 1;
+		for(int i=2; i<=n; i++){
+			printf("%d %d\n",root,i);
+			if(s[i-1] == '1') root = i;
+		}
+	}
+	else puts("-1");
+	return 0;
+}
