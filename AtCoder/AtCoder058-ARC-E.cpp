bcc0921e95c40538c0576e0f5e65406f1377848d
Victor Racso Galvan Oyola <racso_galvan@hotmail.com>
2021-03-09 21:16:21 -0500

    Iroha and Haiku with comments

--- AtCoder/AtCoder058-ARC-E.cpp
+++ AtCoder/AtCoder058-ARC-E.cpp
@@ -1,31 +1,133 @@
 #include<bits/stdc++.h>
 using namespace::std;
 
-const int N = 40 + 5;
+/*
+	Author: Racso Galvan
+
+	Idea:
+
+	- Aho-Corasick & DP problem
+
+	- Compute the complement of the answer: The sequences that don't have XYZ in them
+
+	- Thus, we can just hardcode all the sequences that shouldn't be inside our
+
+	  answer.
+
+	- Then, it reduces to a classic Aho-Corasick problem: Number of sequences
+
+	  that don't contain any pattern given.
+
+	- Complexity: O(n * nodes * E), where nodes is the sum of lengths of the forbidden
+
+	  sequences, which is bounded by 34000.
+
+*/
+
 const int MOD = 1000000000 + 7;
+const int SUML = 34000 + 5;
+const int N = 40 + 5;
+const int E = 10;
+
+int pow_mod(int a, int b){
+	int r = 1;
+	while(b > 0){
+		if(b & 1) r = (1ll * a * r) % MOD;
+		a = (1ll * a * a) % MOD;
+		b >>= 1;
+	}
+	return r;
+}
 
 int n;
-int a[4];
-int C[N];
-int Q[10][10];
-bool vis[N][10][4];
-int memo[N][10][4];
+int a[3];
+int nodes;
+int suf[SUML];
+bool vis[SUML];
+vector<int> act;
+int go[E][SUML];
+int memo[N][SUML];
+vector< vector<int> > pat[3];
 
-void preprocess(){
-	Q[0][0] = 1;
-	for(int i = 1; i <= 7; i++){
-		for(int j = 1; j <= i; j++){
-			for(int k = 1; k <= i; k++){
-				Q[i][j] += Q[i - k][j - 1];
-				if(Q[i][j] >= MOD) Q[i][j] -= MOD;
+void insert(vector<int> &v){
+	int pos = 0;
+	for(int i = 0; i < v.size(); i++){
+		int nxt = v[i];
+		if(go[nxt][pos] == 0){
+			go[nxt][pos] = nodes++;
+		}
+		pos = go[nxt][pos];
+	}
+	vis[pos] = true;
+}
+
+void backtracking(int id, int val){
+	if(val == 0){
+		pat[id].emplace_back(act);
+		return;
+	}
+	for(int i = 0; i < E; i++){
+		if(i + 1 > val) break;
+		act.emplace_back(i);
+		backtracking(id, val - i - 1);
+		act.pop_back();
+	}
+}
+
+void BFS(int src){
+	queue<int> Q;
+	Q.emplace(src);
+	while(!Q.empty()){
+		int u = Q.front(); Q.pop();
+		if(vis[suf[u]]) vis[u] = true;
+		for(int i = 0; i < E; i++){
+			if(go[i][u]){
+				int v = go[i][u];
+				suf[v] = u == 0 ? 0 : go[i][suf[u]];
+				Q.emplace(v);
+			}
+			else{
+				go[i][u] = u == 0 ? 0 : go[i][suf[u]];
 			}
 		}
 	}
+}
 
+void preprocess(){
+	for(int i = 0; i < 3; i++){
+		backtracking(i, a[i]);
+	}
+	nodes = 1;
+	for(vector<int> x : pat[0]){
+		for(vector<int> y : pat[1]){
+			for(vector<int> z : pat[2]){
+				vector<int> cur = x;
+				for(auto w : y) cur.emplace_back(w);
+				for(auto w : z) cur.emplace_back(w);
+				insert(cur);
+			}
+		}
+	}
+	BFS(0);
 }
 
 int solve(){
-	
+	memo[0][0] = 1;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < nodes; j++){
+			for(int c = 0; c < E; c++){
+				if(vis[go[c][j]]) continue;
+				memo[i + 1][go[c][j]] += memo[i][j];
+				if(memo[i + 1][go[c][j]] >= MOD) memo[i + 1][go[c][j]] -= MOD;
+			}
+		}
+	}
+	int ans = pow_mod(10, n);
+	for(int i = 0; i < nodes; i++){
+		ans += MOD - memo[n][i];
+		if(ans >= MOD) ans -= MOD;
+	}
+	return ans;
 }
 
 int main(){
